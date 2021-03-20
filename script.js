1db47f3f3be38b956d04d6b324c817ad098ebd85
Kuldeep Joshi <38746631+cooldeep5897@users.noreply.github.com>
2021-03-20 15:59:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+   function upload(){ 
+        var inputText=document.getElementById('container').value;
+        let url1 = "https://api.textgears.com/spelling?key=1gVny1rfj02gy7kY&text="+inputText+"&language=en-GB";
+        let req1 = new XMLHttpRequest();
+        req1.open("GET",url1);
+        req1.send();
+        req1.onload = ()=>{
+            let parsedResponse1 = JSON.parse(req1.response);
+            console.log(parsedResponse1.response.errors);
+        }
+        console.log(inputText);
+    }
+
 function readFile(input) {
     let file = input.files[0];
   
@@ -6,7 +19,8 @@ function readFile(input) {
     reader.readAsText(file);
   
     reader.onload = function() {
-      document.getElementById('area1').value=reader.result;
+        let ele = document.getElementById('container');
+        ele.innerHTML = reader.result;
     };
   
     reader.onerror = function() {
